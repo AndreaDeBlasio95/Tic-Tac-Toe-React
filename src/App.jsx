76798ce7a016274f896f2560ea298d7eda8f5778
AndreaDeBlasio55 <39781342+AndreaDeBlasio95@users.noreply.github.com>
2024-03-02 16:31:11 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <div id="game-container">
+          <ol>
+            <li>
+              <span>Player 1</span>
+              <span>X</span>
+            </li>
+            <li>
+              <span>Player 2</span>
+              <span>O</span>
+            </li>
+          </ol>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="app-container">
+      <img src="game-logo.png" alt="" />
+      <h2>Tic-Tac-Toe</h2>
+      <p></p>
+    </div>
+  );
+}

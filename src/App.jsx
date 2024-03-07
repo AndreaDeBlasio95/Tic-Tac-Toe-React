8202b0a85fcd414e41ba5b8c7dab8dc73f3ab307
AndreaDeBlasio55 <39781342+AndreaDeBlasio95@users.noreply.github.com>
2024-03-07 09:29:13 +0100

    refactoring to handle the winning condition

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import Header from "./components/Header";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
@@ -17,6 +24,21 @@ function App() {
   const [gameTurns, setGameTurns] = useState([]);
   //const [activePlayer, setActivePlayer] = useState("X");
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquare = gameBoard[combination[0].row][combination[0].col];
+    const secondSquare = gameBoard[combination[1].row][combination[1].col];
+    const thirdSquare = gameBoard[combination[2].row][combination[2].col];
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     /*
@@ -52,7 +74,7 @@ function App() {
               isActive={activePlayer === "O"}
             />
           </ol>
-          <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+          <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
         </div>
         <Log turns={gameTurns} />
       </main>
